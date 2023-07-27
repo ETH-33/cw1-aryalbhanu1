aa42486bc3eb2d9b95a02360091e95be977cc2c0
aryalbhanu1 <aryalbhanu@outlook.com>
2023-07-27 20:42:35 +0545

    hobbyists

--- backend/signin.php
+++ backend/signin.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hobbyists - live your obsession</title>
+    <title>PHP & MySQL Blog Application with Admin Panel</title>
     <!-- CUSTOM STYLESHEET -->
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css">
     <!-- ICONSCOUT CDN-->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <!---------GOOGLE FONT MONTSERRAT--------->
